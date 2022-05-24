78560c57f9d68f9e4c911ae25eb1cda47f3d46be
Jameel <shaikjameel460@gmail.com>
2022-05-24 08:39:02 +0530

    functions

---
+++ functions.py
@@ -0,0 +1,16 @@
+
+#Function is a block of code that performs a specific task
+
+# ex:-Dish Washer
+#
+# Dirty dishes(i/p)
+# return value(o/p)
+#
+# Function:wash_dishes
+# 1.Add water/detergent
+# 2.wash dishes
+# 3.Dry them
+
+#functions make ur code more modular and readable
+
+#problem:you are given two lists of numbers and you need to find total of each of these list
